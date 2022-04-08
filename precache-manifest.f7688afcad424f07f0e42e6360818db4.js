9bea99d13f38c04b1ed14161123121ec5d8e3d0a
Susmita <susmitapatil87@gmail.com>
2022-04-08 22:52:42 +0530

    Updates

---
+++ precache-manifest.f7688afcad424f07f0e42e6360818db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7772d0792b7b4aeb8ab1",
+    "url": "/user-table/static/css/main.79b9e844.chunk.css"
+  },
+  {
+    "revision": "7772d0792b7b4aeb8ab1",
+    "url": "/user-table/static/js/main.7772d079.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/user-table/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "e398943f2a013e6173a5",
+    "url": "/user-table/static/js/2.e398943f.chunk.js"
+  },
+  {
+    "revision": "22fce650460d70cd66f6",
+    "url": "/user-table/static/js/runtime~main.22fce650.js"
+  },
+  {
+    "revision": "0d8ca14a5b3fc0cdca8a35fb0c0357d7",
+    "url": "/user-table/index.html"
+  }
+];
